cc9d474b5325088281e3c6ff6b283225f3a55927
Yevhen <65403158+TheRedRover@users.noreply.github.com>
2021-11-20 01:48:32 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Moodle parser')
-parser.add_argument("--password", type=str, help="Password flag")
-parser.add_argument("--login", type=str, help="Login flag")
-parser.add_argument("--no-logs", type=bool, default=True, help="Show logs in commands line")
-args = parser.parse_args()
-
-print(args.password)
